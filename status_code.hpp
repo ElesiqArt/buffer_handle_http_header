e3102dfaf4068349b244e0e6e5c4df669533b7e9
Grégoire Scano <gregoire.scano@malloc.fr>
2018-09-12 15:27:53 +0800

    Handle status code

---
+++ status_code.hpp
@@ -0,0 +1,125 @@
+#ifndef BUFFER_HANDLE_HTTP_HEADER_STATUS_CODE_HPP
+#define BUFFER_HANDLE_HTTP_HEADER_STATUS_CODE_HPP
+
+#include <cstring> // strlen()
+
+#include <buffer_handle_http_header/type.hpp> // action align case config
+
+namespace buffer_handle_http_header
+{
+  typedef uint16_t status_code_t;
+
+  namespace status_code
+  {
+#define BUFFER_HANDLE_HTTP_INSTALL_STATUS_CODE(name_arg, value_arg, reason_arg)	\
+    struct name_arg							\
+    {									\
+      static constexpr status_code_t value = value_arg;			\
+      static constexpr const char * reason = reason_arg;		\
+    }
+
+#define DEFINE(name_arg, value_arg, reason_arg)				\
+    BUFFER_HANDLE_HTTP_INSTALL_STATUS_CODE(name_arg, value_arg, reason_arg)
+
+    namespace informational
+    {
+      // rfc2616 §6.1.1
+      DEFINE(continue_, 100, "Continue");
+      DEFINE(switching_protocols, 101, "Switching Protocols");
+    };
+
+    namespace success
+    {
+      // rfc2616 §6.1.1
+      DEFINE(ok, 200, "OK");
+      DEFINE(created, 201, "Created");
+      DEFINE(accepted, 202, "Accepted");
+      DEFINE(non_authoritative_information, 203, "Non-Authoritative Information");
+      DEFINE(no_content, 204, "No Content");
+      DEFINE(reset_content, 205, "Reset Content");
+      DEFINE(partial_content, 206, "Partial Content");
+    };
+
+    namespace redirection
+    {
+      // rfc2616 §6.1.1
+      DEFINE(multiple_choice, 300, "Multiple Choices");
+      DEFINE(moved_permanently, 301, "Moved Permanently");
+      DEFINE(found, 302, "Found");
+      DEFINE(see_other, 303, "See Other");
+      DEFINE(not_modified, 304, "Not Modified");
+      DEFINE(use_proxy, 305, "Use Proxy");
+      DEFINE(temporary_redirect, 307, "Temporary Redirect");
+    };
+
+    namespace client_error
+    {
+      // rfc2616 §6.1.1
+      DEFINE(bad_request, 400, "Bad Request");
+      DEFINE(unauthorized, 401, "Unauthorized");
+      DEFINE(payment_required, 402, "Payment Required");
+      DEFINE(forbidden, 403, "Forbidden");
+      DEFINE(not_found, 404, "Not Found");
+      DEFINE(method_not_allowed, 405, "Method Not Allowed");
+      DEFINE(not_acceptable, 406, "Not Acceptable");
+      DEFINE(proxy_authentication_required, 407, "Proxy Authentication Required");
+      DEFINE(request_time_out, 408, "Request Time-out");
+      DEFINE(conflict, 409, "Conflict");
+      DEFINE(gone, 410, "Gone");
+      DEFINE(length_required, 411, "Length Required");
+      DEFINE(precondition_failed, 412, "Precondition Failed");
+      DEFINE(request_entity_too_large, 413, "Request Entity Too Large");
+      DEFINE(request_uri_too_large, 415, "Request-URI Too Large");
+      DEFINE(unsupported_media_type, 416, "Unsupported Media Type");
+      DEFINE(requested_range_not_satisfiable, 417, "Requested range not satisfiable");
+      DEFINE(expectation_failed, 418, "Expectation Failed");
+
+      // rfc6585 §3, §4, §5
+      DEFINE(precondition_required, 428, "Precondition Required");
+      DEFINE(too_many_requests, 429, "Too Many Requests");
+      DEFINE(request_header_fields_too_large, 431, "Request Header Fields Too Large");
+    };
+
+    namespace server_error
+    {
+      // rfc2616 §6.1.1
+      DEFINE(internal_server_error, 500, "Internal Server Error");
+      DEFINE(not_implemented, 501, "Not Implemented");
+      DEFINE(bad_gateway, 502, "Bad Gateway");
+      DEFINE(service_unavailable, 503, "Service Unavailable");
+      DEFINE(gateway_timeout, 504, "Gateway Time-out");
+      DEFINE(http_version_not_supported, 505, "HTTP Version not supported");
+
+      // rfc6585 §6
+      DEFINE(network_authentication_required, 511, "Network Authentication Required");
+    };
+
+#undef DEFINE
+
+    struct rfc2616_t
+    {
+      static const status_code_t any = 999;
+
+      static const std::size_t max_code_digits = 3;
+      static const std::size_t max_reason_length = std::strlen(client_error::requested_range_not_satisfiable::reason);
+
+      static const char * reason(status_code_t code);
+    };
+
+    struct rfc6585_t
+    {
+      static const status_code_t any = 999;
+      static const std::size_t max_code_digits = 3;
+      static const std::size_t max_reason_length = std::strlen(server_error::network_authentication_required::reason);
+
+      static const char * reason(status_code_t code);
+    };
+  };
+
+  template<config Config, class Code, action Action, class Itoa>
+  char * code_(char * buffer, status_code_t value, const Itoa & itoa = Itoa());
+};
+
+#include <buffer_handle_http_header/status_code.hcp>
+
+#endif/*BUFFER_HANDLE_HTTP_HEADER_STATUS_CODE_HPP*/
--- test.cpp
+++ test.cpp
@@ -3,3 +3,75 @@
 #include <catch2/catch.hpp>
 
 #include <buffer_handle_http_header/type.hpp>
+
+#include <buffer_handle_http_header/status_code.hpp>
+
+#include <buffer_handle/adapter/itoa/to_string.hpp> // to_string_t
+
+using namespace buffer_handle_http_header;
+
+#define GIVEN_A_BUFFER(size)			\
+  char buffer[size] = {0};			\
+  char * begin = buffer;			\
+  char * end = buffer;				\
+  GIVEN("A buffer")
+
+SCENARIO("Status code", "[status_code]")
+{
+  using namespace status_code;
+
+  buffer_handle::adapter::itoa::to_string_t itoa;
+
+  WHEN("Static")
+    {
+      std::size_t size = (std::size_t)status_code_<config::static_, rfc2616_t, action::size>(nullptr, success::ok::value, itoa);
+
+      GIVEN("Size")
+	{
+	  GIVEN_A_BUFFER(size)
+	  {
+	    THEN("Prepare")
+	      {
+		end = status_code_<config::static_, rfc2616_t, action::prepare>(begin, success::ok::value, itoa);
+
+		REQUIRE(end - begin == size);
+		REQUIRE(std::string(begin, end) == "200 OK");
+	      }
+	  }
+	}
+    }
+
+  WHEN("Dynamic")
+    {
+      std::size_t size = (std::size_t)status_code_<config::dynamic, rfc2616_t, action::size>(nullptr, rfc2616_t::any, itoa);
+
+      GIVEN("Size")
+	{
+	  GIVEN_A_BUFFER(size)
+	  {
+	    THEN("Prepare")
+	      {
+		end = status_code_<config::dynamic, rfc2616_t, action::prepare>(begin, success::ok::value, itoa);
+
+		REQUIRE(end - begin == size);
+		REQUIRE(std::string(begin, end) == std::to_string(success::ok::value) + std::string(1 + rfc2616_t::max_reason_length - std::strlen(success::ok::reason), ' ') + success::ok::reason);
+
+		THEN("Write")
+		  {
+		    end = status_code_<config::dynamic, rfc2616_t, action::write>(begin, server_error::http_version_not_supported::value, itoa);
+
+		    REQUIRE(end - begin == size);
+		    REQUIRE(std::string(begin, end) == std::to_string(server_error::http_version_not_supported::value) + " " + std::string(rfc2616_t::max_reason_length - std::strlen(server_error::http_version_not_supported::reason), ' ') + server_error::http_version_not_supported::reason);
+		    THEN("Reset")
+		      {
+			end = status_code_<config::dynamic, rfc2616_t, action::write>(begin, client_error::requested_range_not_satisfiable::value, itoa);
+
+			REQUIRE(end - begin == size);
+			REQUIRE(std::string(begin, end) == std::to_string(client_error::requested_range_not_satisfiable::value) + " " + std::string(rfc2616_t::max_reason_length - std::strlen(client_error::requested_range_not_satisfiable::reason), ' ') + client_error::requested_range_not_satisfiable::reason);
+		      }
+		  }
+	      }
+	  }
+	}
+    }
+}
