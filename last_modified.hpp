51aeb7eb938863b11307d1cbdabcfe5504bcf10d
Grégoire Scano <gregoire.scano@malloc.fr>
2018-09-13 16:52:28 +0800

    Handle last modified

---
+++ last_modified.hpp
@@ -0,0 +1,23 @@
+#ifndef BUFFER_HANDLE_HTTP_HEADER_LAST_MODIFIED_HPP
+#define BUFFER_HANDLE_HTTP_HEADER_LAST_MODIFIED_HPP
+
+#include <buffer_handle_http_header/common.hpp> // integral_number_field_t
+#include <buffer_handle_http_header/type.hpp> // action config
+
+namespace buffer_handle_http_header
+{
+  // rfc2616 §14.21
+
+  template<config Config, action Action,
+	   typename Weekday, typename Day, typename Month, typename Year,
+	   typename Hours, typename Minutes, typename Seconds>
+  char * last_modified(char * buffer, Weekday weekday, Day day, Month month, Year year,
+		       Hours hours, Minutes minutes, Seconds seconds);
+
+  template<config Config, action Action>
+  char * last_modified(char * buffer, std::tm value);
+};
+
+#include <buffer_handle_http_header/last_modified.hcp>
+
+#endif/*BUFFER_HANDLE_HTTP_HEADER_LAST_MODIFIED_HPP*/
